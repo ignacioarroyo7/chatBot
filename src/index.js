a0040234604e4c1cbdc50e3bf713dc0a19bdd808
ignacioarroyo7 <ignacioarroyo17@gmail.com>
2022-05-19 20:21:41 -0300

    add middleware

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import express from "express";
+import morgan from "morgan";
+import cors from "cors";
 
 const app = express();
 app.set('port', process.env.PORT || 3000);
@@ -6,4 +8,14 @@ app.set('port', process.env.PORT || 3000);
 
 app.listen(app.get('port'),()=>{
     console.log(`SERVER PORT: ${app.get('port')}`);
-})
+});
+
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/',(req,res)=>{
+    res.send('hola back')
+});
+  
